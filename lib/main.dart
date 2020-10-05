8d01cd8d5f70c9190829089f7ec92f725250528c
CynicalOne <cynicalonemusic@gmail.com>
2020-10-05 14:53:34 -0700

    added some bluetooth functionality app can connect, scan, and monitor status but no ui to show yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:traze/traze_screening.dart';
-
-import 'traze_home.dart';
 import 'traze_login.dart';
 
 void main() {
   runApp(MaterialApp(title: 'Login App', home: LogIn())); //MaterialApp
 
   //runApp(MaterialApp(title: 'Drawer App', home: Home(),
-
   //runApp(MaterialApp(title: 'self screening',home: SelfScreening(),));
 }
