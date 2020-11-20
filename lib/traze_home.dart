d380bad8987f661bb5dbbb76afe865e2351eecf3
Julio Hernandez <julio@julios-air.lan>
2020-11-19 20:26:03 -0800

    Google Map on Home Changed to Statefull/Needs to be changed to heatmap

--- lib/traze_home.dart
+++ lib/traze_home.dart
@@ -9,23 +9,74 @@ import 'package:traze/traze_input_test.dart';
 import 'package:traze/traze_login.dart';
 import 'package:traze/traze_positive_scan.dart';
 import 'package:traze/traze_screening.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  GoogleMapController _controller;
+
   final CameraPosition _initialPosition =
-      CameraPosition(target: LatLng(24.903623, 67.198367));
+      CameraPosition(target: LatLng(34.0522, 118.2437));
+
+  final List<Marker> markers = [];
+
+  addMarker(cordinate) {
+    int id = Random().nextInt(100);
+
+    setState(() {
+      markers
+          .add(Marker(position: cordinate, markerId: MarkerId(id.toString())));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: _initialPosition,
         mapType: MapType.normal,
+        onMapCreated: (controller) {
+          setState(() {
+            _controller = controller;
+          });
+        },
+        markers: markers.toSet(),
+        onTap: (cordinate) {
+          _controller.animateCamera(CameraUpdate.newLatLng(cordinate));
+          addMarker(cordinate);
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _controller.animateCamera(CameraUpdate.zoomOut());
+        },
         child: Icon(Icons.zoom_out),
-      ),
+      ), // This trailing comma makes
       appBar: AppBar(
         title: Text('Heat Map'),
         backgroundColor: Colors.deepOrangeAccent,
@@ -99,6 +150,7 @@ class Home extends StatelessWidget {
           ],
         ),
       ),
+      // auto-formatting nicer for build methods.
     );
   }
 }
